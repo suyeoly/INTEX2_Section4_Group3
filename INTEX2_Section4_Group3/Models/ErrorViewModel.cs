aa90639c9f2f4fa1dc9ca1720cc3b2582a0afd59
suyeol <suyeol@SUYEOL5544>
2024-04-08 10:17:07 -0600

    Add project files.

---
+++ INTEX2_Section4_Group3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INTEX2_Section4_Group3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
